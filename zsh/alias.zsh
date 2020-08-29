e446fbe3e28f8ad3f812f76b21d7b6b793030a77
somit <somitbit@gmail.com>
2020-08-29 11:16:42 +0530

    updates

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,4 +1,5 @@
-
+# Pipe my public key to my clipboard.
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
 # Easier navigation: .., ..., ~ and -
 alias ..="cd .."
