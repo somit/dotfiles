4933bf0a87bbd71d5a7f6b667dbdfdfa11881c2b
Somit Srivastava <somit.pisces@gmail.com>
2020-12-24 22:24:54 +0530

    Font changes

---
+++ fonts.sh
@@ -0,0 +1,4 @@
+https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/complete/Fura%20Mono%20Regular%20Nerd%20Font%20Complete.otf?raw=true
+
+
+https://medium.com/@shivam1/make-your-terminal-beautiful-and-fast-with-zsh-shell-and-powerlevel10k-6484461c6efb
